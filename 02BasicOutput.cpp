90be7df61976978a227ff91ea4830465be16b5f8
Cris Vega <mariacriszabel@gmail.com>
2024-10-05 16:32:57 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "School\t\t\t\t:\tOur Lady of Lourdes College Foundation" <<endl;
+	cout << "Department\t\t\t:\tCollege of Computer Studies" <<endl;
+	cout << "Course & year level\t\t:\tBachelor of Science in Information Technology First year" <<endl;
+	cout << "Permanent Address\t\t:\tPurok 2, Brgy Magang, Daet Camarines Norte" <<endl;
+	
+	return 0;
+}
