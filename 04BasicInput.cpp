9216a869e3d5b852abc8d8ef1bf100b77947e37c
Maria. Criszabel Laborte Vega <mariacriszabel@gmail.com>
2024-10-07 09:41:43 +0800

    Add files via upload

--- 04BasicInput.cpp
+++ 04BasicInput.cpp
@@ -1,1 +1,21 @@
-#
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	string Course, School;
+	
+	cout << "Nickname:";
+	cin >> name;
+	cout << "Course year level:";
+	cin >> Course;
+	cout << "Your School:";
+	cin >> School;
+	
+	cout << "Wow Congrats" << name << "! " << Course << " is a nice course and you are studying'\n";
+	cout << "in " << School << " which is one of the Center of exellence in teritary Education"<<endl;
+	
+	return 0;
+}	
