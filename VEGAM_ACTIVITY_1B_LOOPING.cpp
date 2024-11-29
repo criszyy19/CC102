babf68e33239682ce9d9b3eefdb88efd0075a1ea
Maria. Criszabel Laborte Vega <mariacriszabel@gmail.com>
2024-11-29 14:52:28 +0800

    Add files via upload

---
+++ VEGAM_ACTIVITY_1B_LOOPING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	for (int i = 8; i >= 1; i--) {
+		for (int j = 1; j <= i; j++) {
+			cout << " #* ";
+		}
+		cout << endl;
+	}
+		return 0;
+	}		
